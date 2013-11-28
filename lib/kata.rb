e5a2ca53a8f2d92c53840a79cd57478bcd9554ff
Ed Gibbs <edward_gibbs@yahoo.com>
2013-11-28 08:13:24 -0800

    Add Gemfile creation.

--- lib/kata.rb
+++ lib/kata.rb
@@ -17,5 +17,18 @@ class Kata < Thor
     ruby_version_file = File.new(".ruby-version", "w")
     ruby_version_file.puts("ruby-1.9.3-p392")
     ruby_version_file.close
+
+
+    gemfile = File.new("Gemfile", "w")
+    gemfile_contents = <<-CONTENT
+source "http://rubygems.org"
+
+gem "rspec"
+gem "rspec-given"
+gem "debugger"
+    CONTENT
+
+    gemfile.puts(gemfile_contents)
+    gemfile.close
   end
 end
--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 
 describe Kata do
   let(:kata) { Kata.new }
-  let(:file) { StringIO.new }
 
   describe "#help" do
     it "displays a help message" do
@@ -14,7 +13,7 @@ describe Kata do
   describe "#build" do
     before do
       FileUtils.stub(:mkdir)
-      File.stub(:new).and_return(file)
+      File.stub(:new).and_return(StringIO.new, StringIO.new, StringIO.new)
     end
 
     it "creates new directories" do
@@ -25,6 +24,12 @@ describe Kata do
     end
 
     context "creates a ruby version file for rvm" do
+      let(:ruby_version_file) { StringIO.new }
+
+      before do
+        File.stub(:new).with(".ruby-version", "w").and_return(ruby_version_file)
+      end
+
       it "adds a ruby version file" do
         File.should_receive(:new).with(".ruby-version", "w")
         kata.build "sample_kata"
@@ -32,11 +37,37 @@ describe Kata do
 
       it "writes the version to the file" do
         kata.build "sample_kata"
-        file.string.chomp.should == "ruby-1.9.3-p392"
+        ruby_version_file.string.chomp.should == "ruby-1.9.3-p392"
       end
 
       it "closes the file" do
-        file.should_receive(:close)
+        ruby_version_file.should_receive(:close)
+        kata.build "sample_kata"
+      end
+    end
+
+    context "creates a Gemfile" do
+      let(:gemfile) { StringIO.new }
+
+      before do
+        File.stub(:new).with("Gemfile", "w").and_return(gemfile)
+      end
+
+      it "adds a Gemfile" do
+        File.should_receive(:new).with("Gemfile", "w")
+        kata.build "sample_kata"
+      end
+
+      it "writes the gems in the Gemfile" do
+        kata.build "sample_kata"
+        gemfile.string.should include 'source "http://rubygems.org"'
+        gemfile.string.should include 'gem "rspec"'
+        gemfile.string.should include 'gem "rspec-given"'
+        gemfile.string.should include 'gem "debugger"'
+      end
+
+      it "closes the file" do
+        gemfile.should_receive(:close)
         kata.build "sample_kata"
       end
     end
